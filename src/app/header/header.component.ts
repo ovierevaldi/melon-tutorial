7ee7571aeeac6f6f11dda47e3dc26f8b2c23d4d5
Ebrahim Erik <ovierevaldi@gmail.com>
2020-03-04 16:38:51 +0700

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,7 +8,7 @@ import { TutorialService } from '../tutorial/tutorial.service';
     <div class="container-fluid">
       <div class="col-lg-3 col-md justify-content-center-md pl-0">
         <div class="row">
-          <div class="col-6">         
+          <div class="col-6 pl-0">         
             <img src="assets/images/logo-tutorial.png" alt="" style="width: 100%;">
           </div>
           <div class="col-auto pl-0 mt-1">
